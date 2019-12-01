a096930cae9d27eb0281ff7767021fd3929944ac
Soner Mezgitci <you@example.com>
2019-11-30 19:21:24 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :songs,  only:[:create, :show, :update]
+  resources :genres, only: [:create, :show, :update]
+  resources :artists, only:[:create, :show, :update]
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
